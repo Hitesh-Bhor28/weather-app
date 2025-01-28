f88a76eab8dcfc5ccac35434005177b2154540a3
Hitesh-Bhor28 <hiteshbhor28@gmail.comgit>
2025-01-28 13:24:50 +0530

    Added weather API

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import "./Weather.css";
 import searchIcon from "../assets/search.png";
+
+// Toastify
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import clearIcon from "../assets/clear.png";
 import cloudIcon from "../assets/cloud.png";
@@ -11,34 +15,130 @@ import windIcon from "../assets/wind.png";
 import humidityIcon from "../assets/humidity.png";
 
 const Weather = () => {
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clearIcon,
+    "01n": clearIcon,
+    "02d": cloudIcon,
+    "02n": cloudIcon,
+    "03d": cloudIcon,
+    "03n": cloudIcon,
+    "04d": drizzleIcon,
+    "04n": drizzleIcon,
+    "09d": rainIcon,
+    "09n": rainIcon,
+    "10d": rainIcon,
+    "10n": rainIcon,
+    "13d": snowIcon,
+    "13n": snowIcon,
+  };
+
+  const showToastEnterCity = () => {
+    toast.error("Enter City!", {
+      style: { borderRadius: "15px", background: "#f0f0f0", color: "#333" },
+      position: "top-right",
+      autoClose: 3000,
+      theme: "light",
+    });
+  };
+
+  const showToastApi = () => {
+    toast.error("Error while fetching weather data!", {
+      style: { borderRadius: "15px", background: "#f0f0f0", color: "#333" },
+      position: "top-right",
+      autoClose: 3000,
+      theme: "light",
+    });
+  };
+  const showToastCityNotFound = () => {
+    toast.error("city not found!", {
+      style: { borderRadius: "15px", background: "#f0f0f0", color: "#333" },
+      position: "top-right",
+      autoClose: 3000,
+      theme: "light",
+    });
+  };
+
+  const search = useCallback(async (city) => {
+    if (city === "") {
+      showToastEnterCity();
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_URL}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        showToastCityNotFound();
+        return;
+      }
+
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: allIcons[data.weather[0].icon] || clearIcon,
+      });
+    } catch (error) {
+      console.error("Fetch error:", error);
+      showToastApi();
+    }
+  }, []);
+
+  useEffect(() => {
+    search("nashik");
+  }, [search]);
+
   return (
     <div className="weather">
+      <ToastContainer />
       <div className="search-bar">
-        <input type="text" name="" value="" id="" placeholder="Search" />
-        <img src={searchIcon} alt="search logo" srcset="" />
+        <input type="text" ref={inputRef} placeholder="Search" />
+        <img
+          src={searchIcon}
+          alt="search logo"
+          onClick={() => search(inputRef.current.value)}
+        />
       </div>
 
-      <img src={clearIcon} className="weather-icon" alt="" srcset="" />
-      <p className="temprature">16°c</p>
-      <p className="location">London</p>
+      {weatherData ? (
+        <>
+          <img src={weatherData.icon} className="weather-icon" alt="" />
+          <p
+            className="temperature"
+            style={{
+              color: "#ffff",
+              fontSize: "9vh",
+              lineHeight: "1",
+            }}
+          >
+            {weatherData.temperature}°c
+          </p>
+          <p className="location">{weatherData.location}</p>
 
-      <div className="weather-data">
-        <div className="col">
-          <img src={humidityIcon} alt="" />
-          <div>
-            <p>91%</p>
-            <span>Humidity</span>
-          </div>
-        </div>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidityIcon} alt="" />
+              <div>
+                <p>{weatherData.humidity}</p>
+                <span>Humidity</span>
+              </div>
+            </div>
 
-        <div className="col">
-          <img src={windIcon} alt="" />
-          <div>
-            <p>3.6 Km/h</p>
-            <span>Wind Speed</span>
+            <div className="col">
+              <img src={windIcon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed}</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : null}
     </div>
   );
 };
